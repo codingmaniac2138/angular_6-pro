4142a9ff12edfe4dd1232dc55c7615808b25ce3a
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-08-13 12:00:12 +0530

    added alternative statement for implimenting the active router

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,11 +11,19 @@ export class SidebarComponent implements OnInit {
   currentUrl: string;
 
   constructor(private router: Router) {
+    // it works wih below as well
+    // router.events.subscribe((_:NavigationEnd) => {
+    //   if(!!_.url){
+    //     this.currentUrl = _.url;
+    //     console.log(this.currentUrl);
+    //   }
+
+    // it works like this as well
     router.events.subscribe((_:NavigationEnd) => {
-      if(!!_.url){
-        this.currentUrl = _.url;
+      
+        this.currentUrl = this.router.url;
         console.log(this.currentUrl);
-      }
+      
     });
     console.log("pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp");
     // console.log(router.events);
